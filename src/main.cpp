7d667f5ac55ebeacd363387cc5479fb9150d0f58
puku0x <puku0x@gmail.com>
2014-07-11 14:39:30 +0900

    Fixed a bug and added image undistortion
    Final commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-﻿// OpenCV
+﻿// C++ STL
+#include <iostream>
+#include <fstream>
+
+// OpenCV
 #include <opencv2/opencv.hpp>
 
 // OpenGL
@@ -8,12 +12,13 @@
 #include ".\SimpleAR\MarkerDetector.hpp"
 
 // チェスボードのパラメータ
-#define PAT_ROW    (7)                  // パターンの行数
-#define PAT_COL    (10)                 // パターンの列数
+#define PAT_ROWS   (7)                  // パターンの行数
+#define PAT_COLS   (10)                 // パターンの列数
 #define CHESS_SIZE (24.0)               // パターン一つの大きさ [mm]
 
 // グローバル変数
 cv::VideoCapture cap;
+cv::Mat mapx, mapy;
 CameraCalibration calibration;
 
 // --------------------------------------------------------------------------
@@ -23,9 +28,9 @@ CameraCalibration calibration;
 // --------------------------------------------------------------------------
 Matrix44 buildProjectionMatrix(Matrix33 cameraMatrix, int screen_width, int screen_height)
 {
-    float near = 0.01;  // Near clipping distance
-    float far  = 100;   // Far clipping distance
-    
+    float d_near = 0.01;    // Near clipping distance
+    float d_far = 100.0;    // Far clipping distance
+
     // Camera parameters
     float f_x = cameraMatrix.data[0]; // Focal length in x axis
     float f_y = cameraMatrix.data[4]; // Focal length in y axis (usually the same?)
@@ -33,24 +38,24 @@ Matrix44 buildProjectionMatrix(Matrix33 cameraMatrix, int screen_width, int scre
     float c_y = cameraMatrix.data[5]; // Camera primary point y
 
     Matrix44 projectionMatrix;
-    projectionMatrix.data[0] = - 2.0 * f_x / screen_width;
+    projectionMatrix.data[0] = -2.0 * f_x / screen_width;
     projectionMatrix.data[1] = 0.0;
     projectionMatrix.data[2] = 0.0;
     projectionMatrix.data[3] = 0.0;
-    
+
     projectionMatrix.data[4] = 0.0;
     projectionMatrix.data[5] = 2.0 * f_y / screen_height;
     projectionMatrix.data[6] = 0.0;
     projectionMatrix.data[7] = 0.0;
-    
+
     projectionMatrix.data[8] = 2.0 * c_x / screen_width - 1.0;
     projectionMatrix.data[9] = 2.0 * c_y / screen_height - 1.0;
-    projectionMatrix.data[10] = -( far+near ) / ( far - near );
+    projectionMatrix.data[10] = -(d_far + d_near) / (d_far - d_near);
     projectionMatrix.data[11] = -1.0;
-    
+
     projectionMatrix.data[12] = 0.0;
     projectionMatrix.data[13] = 0.0;
-    projectionMatrix.data[14] = -2.0 * far * near / ( far - near );
+    projectionMatrix.data[14] = -2.0 * d_far * d_near / (d_far - d_near);
     projectionMatrix.data[15] = 0.0;
 
     return projectionMatrix;
@@ -79,8 +84,12 @@ void display(void)
 
     if (cap.isOpened()) {
         // カメラ画像取得
+        cv::Mat image_raw;
+        cap >> image_raw;
+
+        // 歪み補正
         cv::Mat image;
-        cap >> image;
+        cv::remap(image_raw, image, mapx, mapy, cv::INTER_LINEAR);
 
         // カメラ画像(RGB)表示
         cv::Mat rgb;
@@ -130,27 +139,26 @@ void display(void)
         glLineWidth(3.0f);
 
         // ライン頂点配列
-        float lineX[] = {0,0,0,1,0,0};
-        float lineY[] = {0,0,0,0,1,0};
-        float lineZ[] = {0,0,0,0,0,1};
+        float lineX[] = { 0, 0, 0, 1, 0, 0 };
+        float lineY[] = { 0, 0, 0, 0, 1, 0 };
+        float lineZ[] = { 0, 0, 0, 0, 0, 1 };
 
         // 2D平面
         const GLfloat squareVertices[] = { -0.5f, -0.5f,
                                             0.5f, -0.5f,
                                            -0.5f,  0.5f,
-                                            0.5f,  0.5f};
+                                            0.5f,  0.5f };
 
         // 2D平面カラー(RGBA)
-        const GLubyte squareColors[] = {255, 255,   0, 255,
-                                          0, 255, 255, 255,
-                                          0,   0,   0,   0,
-                                        255,   0, 255, 255};
+        const GLubyte squareColors[] = { 255, 255,   0, 255,
+                                           0, 255, 255, 255,
+                                           0,   0,   0,   0,
+                                         255,   0, 255, 255 };
 
         // AR描画
         for (size_t i = 0; i < transformations.size(); i++) {
             // 変換行列
-            const Transformation &transformation = transformations[i];
-            Matrix44 glMatrix = transformation.getMat44();
+            Matrix44 glMatrix = transformations[i].getMat44();
 
             // ビュー行列にロード
             glLoadMatrixf(reinterpret_cast<const GLfloat*>(&glMatrix.data[0]));
@@ -201,13 +209,13 @@ void display(void)
 // キーボードの入力時に呼ばれる関数です
 // 戻り値: なし
 // --------------------------------------------------------------------------
-void key(unsigned char key , int x , int y) {
+void key(unsigned char key, int x, int y) {
     switch (key) {
-        case 0x1b:
-            exit(1);
-            break;
-        default :
-            break;
+    case 0x1b:
+        exit(1);
+        break;
+    default:
+        break;
     }
 }
 
@@ -236,16 +244,21 @@ int main(int argc, char *argv[])
 {
     // カメラ初期化
     if (!cap.open(0)) {
+    //if (!cap.open("tcp://192.168.1.1:5555")) {
         printf("カメラ初期化エラー\n");
         return -1;
     }
 
+    // 試しに1枚
+    cv::Mat frame;
+    cap >> frame;
+
     // ファイル名を指定して開く
-    const char *filename = "camera.xml";
-    FILE *file = fopen(filename, "r");
+    std::string filename("camera.xml");
+    std::fstream file("camera.xml", std::ios::in);
 
     // ファルがない
-    if (!file) {
+    if (!file.is_open()) {
         // 画像
         std::vector<cv::Mat> images;
         std::cout << "Spaceキーを押して画像キャプチャ" << std::endl;
@@ -258,7 +271,6 @@ int main(int argc, char *argv[])
             if (key == 0x1b) break;
 
             // キャプチャ
-            cv::Mat frame;
             cap >> frame;
 
             // グレースケール変換
@@ -266,7 +278,7 @@ int main(int argc, char *argv[])
             cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
 
             // チェスボード検出
-            cv::Size size(PAT_COL, PAT_ROW);
+            cv::Size size(PAT_COLS, PAT_ROWS);
             std::vector<cv::Point2f> corners;
             bool found = cv::findChessboardCorners(gray, size, corners, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE | cv::CALIB_CB_FAST_CHECK);
 
@@ -285,14 +297,14 @@ int main(int argc, char *argv[])
             // 表示
             std::ostringstream stream;
             stream << "Captured " << images.size() << " image(s).";
-            cv::putText(frame, stream.str(), cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0,255,0), 1, CV_AA);
+            cv::putText(frame, stream.str(), cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0), 1, CV_AA);
             cv::imshow("Camera Calibration", frame);
         }
 
         // 十分なサンプルがある
         if (images.size() > 0) {
             // チェスボードのデータ
-            cv::Size size(PAT_COL, PAT_ROW);
+            cv::Size size(PAT_COLS, PAT_ROWS);
             std::vector<std::vector<cv::Point2f>> corners2D;
             std::vector<std::vector<cv::Point3f>> corners3D;
 
@@ -310,9 +322,9 @@ int main(int argc, char *argv[])
                     // チェスボード実寸を代入
                     const float squareSize = CHESS_SIZE;
                     std::vector<cv::Point3f> tmp_corners3D;
-                    for (int j = 0; j < size.height; j++ ) {
+                    for (int j = 0; j < size.height; j++) {
                         for (int k = 0; k < size.width; k++) {
-                            tmp_corners3D.push_back(cv::Point3f((float)(j*squareSize), (float)(k*squareSize), 0.0));
+                            tmp_corners3D.push_back(cv::Point3f((float)(k*squareSize), (float)(j*squareSize), 0.0));
                         }
                     }
                     corners3D.push_back(tmp_corners3D);
@@ -322,7 +334,7 @@ int main(int argc, char *argv[])
             // カメラキャリブレーション行列を推定
             cv::Mat cameraMatrix, distCoeffs;
             std::vector<cv::Mat> rvec, tvec;
-            cv::calibrateCamera(corners3D, corners2D, images[0].size(), cameraMatrix, distCoeffs, rvec, tvec);
+            cv::calibrateCamera(corners3D, corners2D, images[0].size(), cameraMatrix, distCoeffs, rvec, tvec, CV_CALIB_FIX_PRINCIPAL_POINT);
             std::cout << cameraMatrix << std::endl;
             std::cout << distCoeffs << std::endl;
 
@@ -334,10 +346,6 @@ int main(int argc, char *argv[])
 
         // ウィンドウをすべて破棄
         cv::destroyAllWindows();
-    }
-    else {
-        // 閉じる
-        fclose(file);
     }
 
     // XMLファイルを開く
@@ -351,21 +359,25 @@ int main(int argc, char *argv[])
     cv::Mat cameraMatrix, distCoeffs;
     rfs["intrinsic"] >> cameraMatrix;
     rfs["distortion"] >> distCoeffs;
+
+    // 歪み補正マップの生成
+    cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat(), cameraMatrix, frame.size(), CV_32FC1, mapx, mapy);
+    //std::cout << cameraMatrix << std::endl;
+
+    // カメラパラメータの設定
     float fx = cameraMatrix.at<double>(0, 0);
     float fy = cameraMatrix.at<double>(1, 1);
     float cx = cameraMatrix.at<double>(0, 2);
     float cy = cameraMatrix.at<double>(1, 2);
-    float dist[4] = {distCoeffs.at<double>(0), distCoeffs.at<double>(1), distCoeffs.at<double>(2), distCoeffs.at<double>(3)};
-    calibration = CameraCalibration(fx, fy, cx, cy, dist);
+    calibration = CameraCalibration(fx, fy, cx, cy);
 
     // GLUT初期化
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(frame.cols, frame.rows);
     glutCreateWindow("Mastering OpenCV with Practical Computer Vision Project");
     glutDisplayFunc(display);
     glutKeyboardFunc(key);
-    glutReshapeFunc(resize);
     glutIdleFunc(idle);
 
     // シーン消去
